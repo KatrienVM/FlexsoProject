e641db9b266effa78e127f6ad0360d0b1c890076
P2000179097 <katrien.van.melle@student.ehb.be>
2018-12-14 13:01:20 +0000

    Project odata added

--- webapp/serviceBinding.js
+++ webapp/serviceBinding.js
@@ -1,5 +1,5 @@
 function initModel() {
-	var sUrl = "/SAPGateway/sap/opu/odata/sap/YXM_110_MEA_EMP_SRV/";
+	var sUrl = "/SAPGateway/sap/opu/odata/SAP/YXM_089_MAE_PROJ_SRV/";
 	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
 	sap.ui.getCore().setModel(oModel);
 }
